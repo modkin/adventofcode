ac7fdfcb6085240c5adc56929fa551f956a720f2
Dominik Thoennes <dominik.thoennes@fau.de>
2018-12-05 00:20:47 +0100

    day 2.2 finished

--- day2/day2.go
+++ day2/day2.go
@@ -6,6 +6,41 @@ import (
 	"strings"
 )
 
+func FindSpecial(name string) string {
+	file, err := os.Open(name)
+	if err != nil {
+		panic(err)
+	}
+	scanner := bufio.NewScanner(file)
+	var words [][]string
+	for scanner.Scan() {
+		word := scanner.Text()
+		chars := strings.Split(word, "")
+		words = append(words, chars)
+	}
+
+	for _, element := range words {
+		for _, element2 := range words {
+			var matches = 0
+			for index, char1 := range element {
+				if char1 == element2[index] {
+					matches++
+				}
+			}
+			if matches == len(element)-1 {
+				for index, char := range element {
+					if char != element2[index] {
+						result := append(element[0:index], element[index+1:]...)
+						return strings.Join(result, "")
+					}
+				}
+			}
+
+		}
+	}
+	return "Not Found"
+}
+
 func Checksum(name string) int {
 	file, err := os.Open(name)
 	if err != nil {
--- main.go
+++ main.go
@@ -10,4 +10,5 @@ func main() {
 	fmt.Println("Day1.1: ", day1.SumFile("./day1input.txt"))
 	fmt.Println("Day1.2: ", day1.FindFirstTwice("./day1input.txt"))
 	fmt.Println("Day2.1: ", day2.Checksum("./day2input.txt"))
+	fmt.Println("Day2.2: ", day2.FindSpecial("./day2input.txt"))
 }
