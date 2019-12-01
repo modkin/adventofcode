4ac24dd30f74639c822c0cf6e959baa443a856fd
Dominik Thoennes <dominik.thoennes@fau.de>
2019-12-01 18:40:42 +0100

    1.2 done

--- 2019/day1/day1.go
+++ 2019/day1/day1.go
@@ -8,7 +8,8 @@ import (
 )
 
 func main() {
-	var fuelRequirement = 0
+	var fuelRequirement1 = 0
+	var fuelRequirement2 = 0
 
 	file, err := os.Open("2019/day1/input")
 	if err != nil {
@@ -18,9 +19,18 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		i, _ := strconv.Atoi(scanner.Text())
-		fuelRequirement += i/3 - 2
+		fuelRequirement1 += i/3 - 2
+		for i > 0 {
+			i = i/3 - 2
+			if i < 0 {
+				i = 0
+			}
+			fuelRequirement2 += i
+		}
+
 	}
-	fmt.Println("Task 1.1: fuel requirement: ", fuelRequirement)
+	fmt.Println("Task 1.1: fuel requirement: ", fuelRequirement1)
+	fmt.Println("Task 1.2: fuel requirement: ", fuelRequirement2)
 
 	err = file.Close()
 	if err != nil {
