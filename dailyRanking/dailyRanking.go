846fb3f626c5af57d5763d77767bcab9aaf51a48
Dominik Thoennes <dominik.thoennes@fau.de>
2019-12-04 15:54:09 +0100

    trying to get nicer output

---
+++ dailyRanking/dailyRanking.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"adventofcode/utils"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"math"
+	"sort"
+)
+
+type member struct {
+	Name    string
+	timings []int
+}
+
+func genEmptyTimings(length int) []int {
+	timings := make([]int, length)
+	for i := 0; i < length; i++ {
+		timings[i] = math.MaxInt32
+	}
+	return timings
+}
+
+func printRanking(allMember []member) {
+	for i, mem := range allMember {
+		fmt.Println(i+1, " ", mem.Name)
+	}
+}
+
+func main() {
+	jsonByte, _ := ioutil.ReadFile("dailyRanking/678703.json")
+
+	var data map[string]interface{}
+	json.Unmarshal([]byte(jsonByte), &data)
+
+	var allMember []member
+
+	//fmt.Println(data["members"].(map[string]interface{})["428833"])
+
+	//for _, elem := range data["members"].(map[string]interface{}) {
+	//	name := elem.(map[string]interface{})["name"]
+	//	tmp := member{name.(string), genEmptyTimings()}
+	//	allMember = append(allMember, tmp)
+	//}
+	daysDone := 4
+	for _, elem := range data["members"].(map[string]interface{}) {
+		name := elem.(map[string]interface{})["name"]
+		tmp := member{name.(string), genEmptyTimings(daysDone * 2)}
+
+		for i, task := range elem.(map[string]interface{})["completion_day_level"].(map[string]interface{}) {
+			tmp.timings[(utils.ToInt(i)-1)*2] = utils.ToInt(task.(map[string]interface{})["1"].(map[string]interface{})["get_star_ts"].(string))
+			tmp.timings[(utils.ToInt(i)-1)*2+1] = utils.ToInt(task.(map[string]interface{})["2"].(map[string]interface{})["get_star_ts"].(string))
+			daysDone += 1
+		}
+		allMember = append(allMember, tmp)
+
+	}
+
+	memberPoints := make([]member, len(allMember))
+	copy(memberPoints, allMember)
+
+	//fmt.Println(allMember)
+	for k := 0; k < 8; k++ {
+		fmt.Println("Day: ", (k/2)+1, ".", k%2+1)
+		sort.SliceStable(allMember, func(i, j int) bool { return allMember[i].timings[k] < allMember[j].timings[k] })
+		sort.SliceStable(memberPoints, func(i, j int) bool { return memberPoints[i].timings[k] < memberPoints[j].timings[k] })
+		for points, _ := range memberPoints {
+			memberPoints[points].timings[k] = points
+		}
+		printRanking(allMember)
+	}
+	fmt.Println(memberPoints)
+
+}
