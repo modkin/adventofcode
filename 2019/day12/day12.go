29c4f4d464c895f57d24e1bba9a3d46e35d8f1f0
Dominik Thoennes <dominik.thoennes@fau.de>
2019-12-12 10:41:07 +0100

    day 12.2 done but ugly

--- 2019/day12/day12.go
+++ 2019/day12/day12.go
@@ -14,6 +14,26 @@ type Planet struct {
 	vel [3]int
 }
 
+func GCD(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+// find Least Common Multiple (LCM) via GCD
+func LCM(a, b int, integers ...int) int {
+	result := a * b / GCD(a, b)
+
+	for i := 0; i < len(integers); i++ {
+		result = LCM(result, integers[i])
+	}
+
+	return result
+}
+
 func calcEnergy(planets [4]Planet) (energy int) {
 	energy = 0
 	for _, planet := range planets {
@@ -28,7 +48,7 @@ func calcEnergy(planets [4]Planet) (energy int) {
 }
 
 func main() {
-	file, err := os.Open("./testInput1")
+	file, err := os.Open("./input")
 	if err != nil {
 		panic(err)
 	}
@@ -52,41 +72,48 @@ func main() {
 	}
 	fmt.Println(planets)
 
-	states := make(map[[4]Planet]bool)
+	states := make(map[[4]Planet]int)
 	//timesteps := 10 * 4686774924
 	//for time := 0; time < timesteps; time++ {
-	for time := 0; ; time++ {
-		///update velocity
-		for idx, _ := range planets {
-			for _, otherPlanet := range planets {
-				if planets[idx] == otherPlanet {
-					continue
-				}
-				for i := 0; i <= 2; i++ {
-					if planets[idx].pos[i] > otherPlanet.pos[i] {
-						planets[idx].vel[i]--
-					} else if planets[idx].pos[i] < otherPlanet.pos[i] {
-						planets[idx].vel[i]++
+	compPeriond := [3]int{0, 0, 0}
+
+	///update velocity
+	for component := 0; component < 3; component++ {
+		time := 0
+		for {
+			states[planets] = time
+			time++
+			for idx, _ := range planets {
+				for _, otherPlanet := range planets {
+					if planets[idx] == otherPlanet {
+						continue
+					}
+
+					if planets[idx].pos[component] > otherPlanet.pos[component] {
+						planets[idx].vel[component]--
+					} else if planets[idx].pos[component] < otherPlanet.pos[component] {
+						planets[idx].vel[component]++
 					}
 				}
 			}
-		}
-		/// update position
-		for idx, _ := range planets {
-			for i := 0; i <= 2; i++ {
-				planets[idx].pos[i] += planets[idx].vel[i]
+
+			/// update position
+			for idx, _ := range planets {
+				planets[idx].pos[component] += planets[idx].vel[component]
+			}
+
+			if val, ok := states[planets]; ok {
+				fmt.Println("Time: ", val)
+				compPeriond[component] = time
+				break
+			}
+			if time%10 == 0 {
+				fmt.Println(time)
 			}
 		}
-
-		if _, ok := states[planets]; ok {
-			fmt.Println("time: ", time)
-			return
-		}
-		states[planets] = true
-		if time%1000000 == 0 {
-			fmt.Println(time)
-		}
 	}
+	fmt.Println(compPeriond)
+	fmt.Println(LCM(compPeriond[0], compPeriond[1], compPeriond[2]))
 
 	fmt.Println(planets)
 
