fd61c099b6e80fc02dcd284d3b9a13d802c1abc2
Dominik Thoennes <dominik.thoennes@fau.de>
2019-12-17 14:43:54 +0100

    16.2 works but needs cleanup

--- 2019/day16/day16.go
+++ 2019/day16/day16.go
@@ -4,6 +4,7 @@ import (
 	"adventofcode/utils"
 	"fmt"
 	"io/ioutil"
+	"math"
 	"strings"
 )
 
@@ -23,24 +24,64 @@ func main() {
 		panic(err)
 	}
 	inputString := strings.TrimSuffix(string(inputByte), "\n")
+	inputString = strings.Repeat(inputString, 10_000)
 	inputSplit := strings.Split(string(inputString), "")
 	input := make([]int, len(inputSplit))
 	for i, char := range inputSplit {
 		input[i] = utils.ToInt(char)
 	}
 
-	nextInput := make([]int, len(input))
-	for phase := 0; phase < 100; phase++ {
-		for digit := 0; digit < len(input); digit++ {
-			factor := genPattern(digit + 1)
-			result := 0
-			for i := 0; i < len(input); i++ {
-				result += input[i] * factor()
-			}
-			nextInput[digit] = utils.IntAbs(result % 10)
-		}
-		input = nextInput
+	idx := input[:7]
+	fmt.Println(idx)
+	idxStart := 0
+	for i := 0; i < 7; i++ {
+		idxStart += idx[i] * int(math.Pow(10, float64(6-i)))
 	}
-	fmt.Println(input[:8])
+
+	matrix := make([]int64, len(input[idxStart:]))
+
+	for i := 0; i < len(matrix); i++ {
+		matrix[i] = 1
+	}
+	for phase := 0; phase < 99; phase++ {
+		for i := 1; i < len(matrix); i++ {
+			matrix[i] = (matrix[i] + matrix[i-1]) % 10
+		}
+	}
+	for i := 0; i < len(matrix); i++ {
+		if matrix[i] < 0 {
+			panic("smaller 0")
+		}
+	}
+
+	for i := 0; i < 8; i++ {
+		result := int64(0)
+		for j, elem := range input[idxStart+i:] {
+			result += matrix[j] * int64(elem)
+		}
+		fmt.Println(result)
+	}
+
+	//
+	//fmt.Println(idxStart)
+	//nextInput := make([]int, len(input))
+	//for phase := 0; phase < 100; phase++ {
+	//	t0 := time.Now()
+	//	for digit := idxStart; digit < len(input); digit++ {
+	//		//factor := genPattern(digit + 1)
+	//		result := 0
+	//		for i := idxStart; i < len(input); i++ {
+	//			if i >= digit {
+	//				result += input[i] * idxStart
+	//			}
+	//		}
+	//		//result *= 10_000
+	//		nextInput[digit] = utils.IntAbs(result % 10)
+	//	}
+	//	input = nextInput
+	//	fmt.Println(phase, " took: ", time.Now().Sub(t0))
+	//}
+
+	//fmt.Println(input[idxStart:idxStart+8])
 
 }
