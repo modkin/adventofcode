9dca4b3921b39eea0bf590010070db822db8fa9c
Dominik Thoennes <dominik.thoennes@fau.de>
2019-12-17 20:20:04 +0100

    day 17.1 done

---
+++ 2019/day17/day17.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"adventofcode/2019/computer"
+	"adventofcode/utils"
+	"fmt"
+	"io/ioutil"
+	"math"
+	"strings"
+)
+
+func printPaintMap(paintMap map[[2]int]string) {
+	maxX, maxY := math.MinInt32, math.MinInt32
+	for pos, _ := range paintMap {
+		if pos[0] > maxX {
+			maxX = pos[0]
+		}
+
+		if pos[1] > maxY {
+			maxY = pos[1]
+		}
+	}
+	for y := 0; y <= maxY; y++ {
+		for x := 0; x < maxX; x++ {
+			fmt.Print(paintMap[[2]int{x, y}])
+		}
+		fmt.Println()
+	}
+}
+
+func findItersections(paintMap map[[2]int]string) (result int) {
+	maxX, maxY := math.MinInt32, math.MinInt32
+	for pos, _ := range paintMap {
+		if pos[0] > maxX {
+			maxX = pos[0]
+		}
+
+		if pos[1] > maxY {
+			maxY = pos[1]
+		}
+	}
+	for y := 1; y <= maxY-1; y++ {
+		for x := 1; x < maxX-1; x++ {
+			if paintMap[[2]int{x, y}] == "#" {
+				if paintMap[[2]int{x - 1, y}] == "#" && paintMap[[2]int{x + 1, y}] == "#" && paintMap[[2]int{x, y - 1}] == "#" && paintMap[[2]int{x, y + 1}] == "#" {
+					paintMap[[2]int{x, y}] = "O"
+					result += y * x
+				}
+			}
+		}
+	}
+	return
+}
+
+func main() {
+	content, err := ioutil.ReadFile("./input")
+	if err != nil {
+		panic(err)
+	}
+	contentString := strings.Split(string(content), ",")
+	intcode := make([]int64, len(contentString))
+	for pos, elem := range contentString {
+		intcode[pos] = utils.ToInt64(elem)
+	}
+	inputCh := make(chan int64)
+	outputCh := make(chan int64)
+	quit := make(chan bool)
+
+	shipMap := make(map[[2]int]string)
+
+	go computer.ProcessIntCode(intcode, inputCh, outputCh, quit)
+
+	running := true
+	x := 0
+	y := 0
+	for running {
+		select {
+		case input := <-outputCh:
+			if input == 10 {
+				y++
+				x = 0
+			} else {
+				shipMap[[2]int{x, y}] = string(rune(input))
+				x++
+			}
+		case <-quit:
+			running = false
+		}
+	}
+	task1 := findItersections(shipMap)
+	printPaintMap(shipMap)
+	fmt.Println("Task 17.1: ", task1)
+}
