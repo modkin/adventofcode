be8ec34c8bb789dfbab891e52dac35908dfa5640
Dominik Thoennes <dominik.thoennes@fau.de>
2019-12-17 21:22:11 +0100

    moves but not correct yet

--- 2019/day17/day17.go
+++ 2019/day17/day17.go
@@ -52,6 +52,26 @@ func findItersections(paintMap map[[2]int]string) (result int) {
 	return
 }
 
+func runCamera(shipMap map[[2]int]string, outputCh <-chan int64, quit <-chan bool) {
+	running := true
+	x := 0
+	y := 0
+	for running {
+		select {
+		case input := <-outputCh:
+			if input == 10 {
+				y++
+				x = 0
+			} else {
+				shipMap[[2]int{x, y}] = string(rune(input))
+				x++
+			}
+		case <-quit:
+			running = false
+		}
+	}
+}
+
 func main() {
 	content, err := ioutil.ReadFile("./input")
 	if err != nil {
@@ -70,24 +90,52 @@ func main() {
 
 	go computer.ProcessIntCode(intcode, inputCh, outputCh, quit)
 
-	running := true
-	x := 0
-	y := 0
-	for running {
-		select {
-		case input := <-outputCh:
-			if input == 10 {
-				y++
-				x = 0
-			} else {
-				shipMap[[2]int{x, y}] = string(rune(input))
-				x++
-			}
-		case <-quit:
-			running = false
+	runCamera(shipMap, outputCh, quit)
+	task1 := findItersections(shipMap)
+	//printPaintMap(shipMap)
+	fmt.Println("Task 17.1: ", task1)
+
+	maxX, maxY := math.MinInt32, math.MinInt32
+	for pos, _ := range shipMap {
+		if pos[0] > maxX {
+			maxX = pos[0]
+		}
+		if pos[1] > maxY {
+			maxY = pos[1]
 		}
 	}
-	task1 := findItersections(shipMap)
+
+	intcode[0] = 2
+	go computer.ProcessIntCode(intcode, inputCh, outputCh, quit)
+	//for i := 0; i < maxX*maxY; i++ {
+	//	<-outputCh
+	//}
+	//runCamera(shipMap,outputCh,quit)
+
+	mainProgramm := []rune("A\n")
+	aProgramm := []rune("R,8\n8\n8\n")
+	videoFeed := []rune("n\n")
+	var total []rune
+	total = append(append(mainProgramm, aProgramm...), videoFeed...)
+	counter := 0
+	running := true
+	for running {
+		select {
+		case <-outputCh:
+		case <-quit:
+			running = false
+		case inputCh <- int64(total[counter]):
+			fmt.Println(string(total[counter]), ": ", total[counter])
+			counter++
+			if counter == len(total) {
+				running = false
+			}
+		}
+	}
+
+	shipMap = make(map[[2]int]string)
+
+	runCamera(shipMap, outputCh, quit)
+	findItersections(shipMap)
 	printPaintMap(shipMap)
-	fmt.Println("Task 17.1: ", task1)
 }
