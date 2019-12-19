d6de3199035e5ee6ba1977052a7111b7c4f327c9
Dominik Thoennes <dominik.thoennes@fau.de>
2019-12-19 08:16:28 +0100

    slow but possibly possible

--- 2019/day19/day19.go
+++ 2019/day19/day19.go
@@ -21,8 +21,6 @@ func main() {
 
 	//shipMap := make(map[[2]int]string)
 
-	//var beam [200][200]int
-
 	counter := 0
 	for x := 0; x < 50; x++ {
 		for y := 0; y < 50; y++ {
@@ -39,6 +37,46 @@ func main() {
 
 		}
 	}
-	fmt.Println(counter)
+	fmt.Println("Task 18.1: ", counter)
 
+	possibleStarts := make(map[[2]int]bool)
+	var beam [10000][10000]int
+	stop := false
+	xLower, xUpper := 0, 100
+	for y := 0; y < 10000; y++ {
+		if stop {
+			break
+		}
+		for x := 0; x < 10000; x++ {
+			if x < xLower || x > xUpper {
+				continue
+			}
+			inputCh := make(chan int64)
+			outputCh := make(chan int64)
+			quit := make(chan bool)
+			go computer.ProcessIntCode(intcode, inputCh, outputCh, quit)
+			inputCh <- int64(x)
+			inputCh <- int64(y)
+			out := <-outputCh
+			beam[x][y] = int(out)
+			if x > 5 && out == 1 && beam[x-1][y] == 0 {
+				xLower = x - 5
+			}
+			if out == 1 && beam[x+1][y] == 0 {
+				xUpper = x + 5
+			}
+			if x >= 99 {
+				if beam[x-99][y] == 1 {
+					possibleStarts[[2]int{x - 99, y}] = true
+				}
+			}
+			if _, ok := possibleStarts[[2]int{x, y - 00}]; ok {
+				fmt.Println(x, " ", y)
+				stop = true
+				break
+			}
+			//fmt.Print(out)
+		}
+		//fmt.Println()
+	}
 }
