af5c372bcff273aa897194b5561fa4917b201008
Dominik Thoennes <dominik.thoennes@fau.de>
2020-11-02 16:47:41 +0100

    day2.2

--- 2015/day2/day2.go
+++ 2015/day2/day2.go
@@ -15,21 +15,32 @@ func main() {
 	}
 
 	scanner := bufio.NewScanner(file)
-	total := 0
+	totalPaper := 0
+	totalRibbon := 0
 	for scanner.Scan() {
 		dimensions := strings.Split(scanner.Text(), "x")
 		x, y, z := utils.ToInt(dimensions[0]), utils.ToInt(dimensions[1]), utils.ToInt(dimensions[2])
 		XY := x * y
 		YZ := y * z
 		XZ := x * z
-		total += 2*XY + 2*YZ + 2*XZ
+		totalPaper += 2*XY + 2*YZ + 2*XZ
 		if XY <= YZ && XY <= XZ {
-			total += XY
+			totalPaper += XY
 		} else if YZ <= XZ {
-			total += YZ
+			totalPaper += YZ
 		} else {
-			total += XZ
+			totalPaper += XZ
+		}
+
+		totalRibbon += x * y * z
+		if x > y && x > z {
+			totalRibbon += 2*y + 2*z
+		} else if y > z {
+			totalRibbon += 2*x + 2*z
+		} else {
+			totalRibbon += 2*x + 2*y
 		}
 	}
-	fmt.Println("Task 2.1: ", total)
+	fmt.Println("Task 2.1: ", totalPaper)
+	fmt.Println("Task 2.2: ", totalRibbon)
 }
