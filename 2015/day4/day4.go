56a09124cf3f2e971c0a07f876b7b3e19100b17d
Dominik Thoennes <dominik.thoennes@fau.de>
2020-11-04 21:58:55 +0100

    2015 day 4.2

--- 2015/day4/day4.go
+++ 2015/day4/day4.go
@@ -10,13 +10,24 @@ func main() {
 
 	input := []byte("ckczppom")
 	salt := 1
+	first := false
+	second := false
 	for true {
 		testInput := append(input, []byte(strconv.Itoa(salt))...)
 		hash := md5.Sum(testInput)
-		if hash[0] == byte(0) && hash[1] == byte(0) && hash[2] < uint8(16) {
+		if hash[0] == byte(0) && hash[1] == byte(0) && hash[2] < uint8(16) && !first {
 
 			fmt.Println("Task 4.1:", salt)
 			//fmt.Println(hex.EncodeToString(hash[:]))
+			first = true
+		}
+		if hash[0] == byte(0) && hash[1] == byte(0) && hash[2] == byte(0) && !second {
+
+			fmt.Println("Task 4.2:", salt)
+			//fmt.Println(hex.EncodeToString(hash[:]))
+			second = true
+		}
+		if first && second {
 			break
 		}
 		salt += 1
