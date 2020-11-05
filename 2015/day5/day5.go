cc432fbb528556c3a5bf79fc85fa027819fdb5d4
Dominik Thoennes <dominik.thoennes@fau.de>
2020-11-05 19:27:02 +0100

    day 5.2 done

--- 2015/day5/day5.go
+++ 2015/day5/day5.go
@@ -48,4 +48,40 @@ func main() {
 		nice_strings += 1
 	}
 	fmt.Println("Task 5.1:", nice_strings)
+	file2, err := os.Open("2015/day5/input")
+	if err != nil {
+		panic(err)
+	}
+	scanner = bufio.NewScanner(file2)
+	nice_strings = 0
+	for scanner.Scan() {
+		word := scanner.Text()
+		double_strings := make(map[[2]byte]int)
+		wordByte := []byte(word)
+		foundDouble := false
+		foundRepeatWithBetween := false
+		for i := 0; i < len(wordByte); i++ {
+			if i < len(wordByte)-1 {
+				newString := [2]byte{wordByte[i], wordByte[i+1]}
+				if idx, ok := double_strings[newString]; ok {
+					if idx < i-1 {
+						foundDouble = true
+					}
+				} else {
+					double_strings[newString] = i
+				}
+			}
+			if i > 1 {
+				if wordByte[i] == wordByte[i-2] {
+					foundRepeatWithBetween = true
+				}
+			}
+
+		}
+
+		if foundDouble && foundRepeatWithBetween {
+			nice_strings += 1
+		}
+	}
+	fmt.Println("Task 5.2:", nice_strings)
 }
