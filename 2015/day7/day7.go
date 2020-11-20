8a71826c9c95061e2feb2c874ec5fb8734c7850c
Dominik Thoennes <dominik.thoennes@fau.de>
2020-11-20 10:17:27 +0100

    needs optimization

--- 2015/day7/day7.go
+++ 2015/day7/day7.go
@@ -3,6 +3,7 @@ package main
 import (
 	"adventofcode/utils"
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 	"strings"
@@ -10,38 +11,72 @@ import (
 
 func main() {
 
-	file, err := os.Open("2015/day6/input")
+	file, err := os.Open("2015/day7/input.txt")
 	if err != nil {
 		panic(err)
 	}
 
-	wires := make(map[string]func() uint8)
+	wires := make(map[string]func() uint16)
+	wireCache := make(map[string]uint16)
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 
 		line := scanner.Text()
 		inst := strings.Split(line, " ")
 		if strings.Contains(line, "RSHIFT") {
-			tmp := func() uint8 {
-				return wires[inst[0]]() >> uint(utils.ToInt(inst[2]))
+			tmp := func() uint16 {
+				if val, ok := wireCache[inst[0]]; ok {
+					return val >> uint(utils.ToInt(inst[2]))
+				} else {
+					return wires[inst[0]]() >> uint(utils.ToInt(inst[2]))
+				}
 			}
 			wires[inst[4]] = tmp
 		} else if strings.Contains(line, "LSHIFT") {
-
+			tmp := func() uint16 {
+				return wires[inst[0]]() << uint(utils.ToInt(inst[2]))
+			}
+			wires[inst[4]] = tmp
 		} else if strings.Contains(line, "OR") {
-
+			tmp := func() uint16 {
+				return wires[inst[0]]() | wires[inst[2]]()
+			}
+			wires[inst[4]] = tmp
 		} else if strings.Contains(line, "AND") {
+			val, err := strconv.Atoi(inst[0])
+			var tmp func() uint16
+			if err == nil {
+				tmp = func() uint16 {
+					return uint16(val) & wires[inst[2]]()
+				}
+			} else {
+				tmp = func() uint16 {
+					return wires[inst[0]]() & wires[inst[2]]()
+				}
+			}
 
+			wires[inst[4]] = tmp
 		} else if strings.Contains(line, "NOT") {
-
+			tmp := func() uint16 {
+				return ^wires[inst[1]]()
+			}
+			wires[inst[3]] = tmp
+		} else {
+			tmp := func() uint16 {
+				val, err := strconv.Atoi(inst[0])
+				if err == nil {
+					return uint16(val)
+				} else {
+					return wires[inst[0]]()
+				}
+			}
+			wires[inst[2]] = tmp
 		}
 	}
 
-	source := inst[0]
-
-	tmp := func() uint8 {
-		return 8
-	}
-	wires["b"] = tmp
-	wires["b"]()
+	//for key, val := range wires {
+	//	fmt.Println(key, val())
+	//}
+	//fmt.Println(wires["z"]())
+	fmt.Println("Task 7.1:", wires["a"]())
 }
