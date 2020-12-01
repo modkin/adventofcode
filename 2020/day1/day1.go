77c4b6a90215a4118f000860bfea14c4b2e57a1a
Dominik Thoennes <dominik.thoennes@fau.de>
2020-12-01 06:19:16 +0100

    2020 day 1.2 done

--- 2020/day1/day1.go
+++ 2020/day1/day1.go
@@ -14,14 +14,22 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	var task1, task2 int
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		next, _ := strconv.Atoi(scanner.Text())
 		for _, elem := range numbers {
 			if next+elem == 2020 {
-				fmt.Println(next * elem)
+				task1 = next * elem
+			}
+			for _, elem2 := range numbers {
+				if next+elem+elem2 == 2020 {
+					task2 = next * elem * elem2
+				}
 			}
 		}
 		numbers = append(numbers, next)
 	}
+	fmt.Println("Task 1.1:", task1)
+	fmt.Println("Task 1.2:", task2)
 }
