b4ee6d21a61b7620ec2e1a5a595f1193ced2d652
Dominik Thoennes <dominik.thoennes@fau.de>
2020-12-03 06:38:01 +0100

    cleanup day 3

--- 2020/day3/day3.go
+++ 2020/day3/day3.go
@@ -4,37 +4,30 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strings"
 )
 
 func main() {
-
-	grid := make(map[[2]int]string)
+	var grid []string
 	file, err := os.Open("2020/day3/input")
 	if err != nil {
 		panic(err)
 	}
 	scanner := bufio.NewScanner(file)
-	row := 0
 	maxX := 0
 	for scanner.Scan() {
-		line := strings.Split(scanner.Text(), "")
-		for i, elem := range line {
-			grid[[2]int{i, row}] = elem
-			if i > maxX {
-				maxX = i
-			}
+		grid = append(grid, scanner.Text())
+		if length := len(grid[len(grid)-1]); length > maxX {
+			maxX = length
 		}
-		row++
 	}
 
 	slope := func(xStep int, yStep int) int {
 		x, trees := 0, 0
-		for y := 0; y < row; y = y + yStep {
-			if grid[[2]int{x, y}] == "#" {
+		for y := 0; y < len(grid); y = y + yStep {
+			if string(grid[y][x]) == "#" {
 				trees++
 			}
-			x = (x + xStep) % (maxX + 1)
+			x = (x + xStep) % (maxX)
 		}
 		return trees
 	}
