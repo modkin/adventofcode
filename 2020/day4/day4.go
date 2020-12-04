bd01110f53f7114591c2af020d00b955f9ffe481
Dominik Thoennes <dominik.thoennes@fau.de>
2020-12-04 06:32:53 +0100

    day 4.2 done but quite ugly

--- 2020/day4/day4.go
+++ 2020/day4/day4.go
@@ -5,6 +5,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -16,7 +17,9 @@ func main() {
 	}
 	required := []string{"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
 	var fields []string
+	var fields2 []string
 	valid := 0
+	valid2 := 0
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := strings.Split(scanner.Text(), " ")
@@ -31,14 +34,83 @@ func main() {
 				valid++
 			}
 			fields = []string{}
+			all2 := true
+			for _, r := range required {
+				if !utils.SliceContains(fields2, r) {
+					all2 = false
+				}
+			}
+			if all2 {
+				valid2++
+			}
+			fields2 = []string{}
 
 		} else {
 			for _, elem := range line {
 				field := strings.Split(elem, ":")[0]
 				fields = append(fields, field)
+				data := strings.Split(elem, ":")[1]
+				if field == "byr" {
+					dataInt, _ := strconv.Atoi(data)
+					if dataInt < 1920 || dataInt > 2002 {
+						continue
+					}
+				}
+				if field == "iyr" {
+					dataInt, _ := strconv.Atoi(data)
+					if dataInt < 2010 || dataInt > 2020 {
+						continue
+					}
+				}
+				if field == "eyr" {
+					dataInt, _ := strconv.Atoi(data)
+					if dataInt < 2020 || dataInt > 2030 {
+						continue
+					}
+				}
+				if field == "hgt" {
+					dataInt, _ := strconv.Atoi(data[0 : len(data)-2])
+					if string(data[len(data)-1]) == "m" {
+						if dataInt < 150 || dataInt > 193 {
+							continue
+						}
+					} else {
+						if dataInt < 59 || dataInt > 76 {
+							continue
+						}
+					}
+				}
+				if field == "hcl" {
+					if string(data[0]) != "#" {
+						continue
+					}
+					if len(data[1:]) != 6 {
+						continue
+						//TODO: missing
+					}
+				}
+				if field == "ecl" {
+					possible := []string{"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}
+					check := false
+					for _, p := range possible {
+						if data == p {
+							check = true
+						}
+					}
+					if !check {
+						continue
+					}
+				}
+				if field == "pid" {
+					if len(data) != 9 {
+						continue
+					}
+				}
+				fields2 = append(fields2, field)
 			}
 		}
 
 	}
 	fmt.Println(valid)
+	fmt.Println(valid2)
 }
