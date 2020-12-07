7c7eefc0d0f2d6065c79c9295d9e474893f69a2c
Dominik Thoennes <dominik.thoennes@fau.de>
2020-12-07 14:53:58 +0100

    improve template

--- utils/template.go
+++ utils/template.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"io/ioutil"
-	"os"
 	"strings"
 )
 
@@ -18,11 +17,7 @@ func main() {
 		fmt.Println(elem)
 	}
 
-	file, err := os.Open("2020/day1/input")
-	if err != nil {
-		panic(err)
-	}
-	scanner := bufio.NewScanner(file)
+	scanner := bufio.NewScanner(OpenFile("2020/day1/input"))
 	for scanner.Scan() {
 		foobar := strings.Split(scanner.Text(), ",")
 		fmt.Println(foobar)
