a173b8680b66c216972fca77fbd359ccd2ca3e04
Dominik Thoennes <dominik.thoennes@fau.de>
2020-12-08 06:32:22 +0100

    cleanup day 8

--- 2020/day8/day8.go
+++ 2020/day8/day8.go
@@ -13,22 +13,17 @@ type instruction struct {
 }
 
 func runProgramm(instList []instruction) (int, bool) {
-	visited := make(map[int]bool)
+	visited := map[int]bool{0: true}
 	inf := false
-	acc := 0
-	pos := 0
-	visited[0] = true
+	acc, pos := 0, 0
 	for {
 		current := instList[pos]
 		if current.inst == "acc" {
 			acc += current.arg
 			pos++
-
-		}
-		if current.inst == "jmp" {
+		} else if current.inst == "jmp" {
 			pos += current.arg
-		}
-		if current.inst == "nop" {
+		} else if current.inst == "nop" {
 			pos++
 		}
 		if visited[pos] {
@@ -43,38 +38,34 @@ func runProgramm(instList []instruction) (int, bool) {
 	return acc, inf
 }
 
+func change(input string) string {
+	if input == "nop" {
+		return "jmp"
+	} else {
+		return "nop"
+	}
+}
+
 func main() {
-
 	instList := make([]instruction, 0)
-
 	scanner := bufio.NewScanner(utils.OpenFile("2020/day8/input"))
 	for scanner.Scan() {
 		line := strings.Split(scanner.Text(), " ")
 		instList = append(instList, instruction{line[0], utils.ToInt(line[1])})
 	}
-	//fmt.Println(instList)
 
 	acc, _ := runProgramm(instList)
 	fmt.Println("Task 8.1", acc)
+
 	for i, elem := range instList {
-		if elem.inst == "jmp" {
-			instList[i] = instruction{"nop", elem.arg}
+		if elem.inst == "jmp" || elem.inst == "nop" {
+			instList[i] = instruction{change(elem.inst), elem.arg}
 			acc, inf := runProgramm(instList)
 			if !inf {
 				fmt.Println("Task 8.2", acc)
 				break
 			} else {
-				instList[i] = instruction{"jmp", elem.arg}
-			}
-		}
-		if elem.inst == "nop" {
-			instList[i] = instruction{"jmp", elem.arg}
-			acc, inf := runProgramm(instList)
-			if !inf {
-				fmt.Println("Task 8.2", acc)
-				break
-			} else {
-				instList[i] = instruction{"nop", elem.arg}
+				instList[i] = instruction{elem.inst, elem.arg}
 			}
 		}
 	}
