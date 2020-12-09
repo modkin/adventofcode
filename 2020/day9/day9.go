4f2ef72158998e523846969fd0daf8d043022e8f
Dominik Thoennes <dominik.thoennes@fau.de>
2020-12-09 06:24:34 +0100

    day 9.2

--- 2020/day9/day9.go
+++ 2020/day9/day9.go
@@ -4,6 +4,7 @@ import (
 	"adventofcode/utils"
 	"bufio"
 	"fmt"
+	"math"
 )
 
 func checkNumber(previous []int, current int) bool {
@@ -17,22 +18,52 @@ func checkNumber(previous []int, current int) bool {
 	return false
 }
 
+func sumMinMax(list []int) int {
+	min, max := math.MaxInt32, 0
+	for _, elem := range list {
+		if elem > max {
+			max = elem
+		}
+		if elem < min {
+			min = elem
+		}
+	}
+	return min + max
+}
+
 func main() {
 	previous := make([]int, 0)
+	allNumbers := make([]int, 0)
 	scanner := bufio.NewScanner(utils.OpenFile("2020/day9/input"))
-	count := 0
+	count, target := 0, 0
 	for scanner.Scan() {
+		allNumbers = append(allNumbers, utils.ToInt(scanner.Text()))
 		if count <= 25 {
 			previous = append(previous, utils.ToInt(scanner.Text()))
 			count++
 		} else {
 			current := utils.ToInt(scanner.Text())
 			if !checkNumber(previous, current) {
-				fmt.Println(current)
+				target = current
 				break
 			}
 			previous = append(previous[1:], current)
 		}
-
+	}
+	fmt.Println("Task 9.1:", target)
+	currentStart := 0
+outer:
+	for {
+		sum := allNumbers[currentStart]
+		for i := 1; i < target; i++ {
+			sum += allNumbers[currentStart+i]
+			if sum == target {
+				fmt.Println("Task 9.2:", sumMinMax(allNumbers[currentStart:currentStart+i+1]))
+				break outer
+			} else if sum > target {
+				currentStart++
+				break
+			}
+		}
 	}
 }
