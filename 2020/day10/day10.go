065c67992026d6c9ff0fa98a5dd764e66782d4a1
Dominik Thoennes <dominik.thoennes@fau.de>
2020-12-10 07:10:06 +0100

    recursive solution

--- 2020/day10/day10.go
+++ 2020/day10/day10.go
@@ -7,10 +7,33 @@ import (
 	"sort"
 )
 
+func findNext(index int, jolt int, adapters []int, target int) int {
+	nOA := 0
+	for i := 1; i <= 3; i++ {
+		if jolt+3 == target {
+			nOA++
+			break
+		}
+		tmp := index + i
+		if tmp > len(adapters)-1 {
+			continue
+		}
+		nextJolt := adapters[tmp]
+		if nextJolt <= jolt+3 {
+			if i == 3 {
+				fmt.Println("bla")
+			}
+			nOA += findNext(tmp, nextJolt, adapters, target)
+		}
+
+	}
+	return nOA
+}
+
 func main() {
 
 	adapters := make([]int, 0)
-	scanner := bufio.NewScanner(utils.OpenFile("2020/day10/input"))
+	scanner := bufio.NewScanner(utils.OpenFile("2020/day10/testinput"))
 	for scanner.Scan() {
 		adapters = append(adapters, utils.ToInt(scanner.Text()))
 	}
@@ -22,5 +45,19 @@ func main() {
 		currentJolt = elem
 	}
 	diffs[2]++
+	fmt.Println(diffs)
 	fmt.Println(diffs[0] * diffs[2])
+
+	foo := []int{0}
+	adapters = append(foo, adapters...)
+	targetJolt := adapters[len(adapters)-1] + 3
+	numberOfArrangements := findNext(0, 0, adapters, targetJolt)
+	//for {
+	//
+	//	for i:= 3; i >= 1; i-- {
+	//
+	//		if adapters[tmp]
+	//	}
+	//}
+	fmt.Println(numberOfArrangements)
 }
