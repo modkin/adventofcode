319a1288f1249c9e2ac16fda014beb518d5a8b0c
Dominik Thoennes <dominik.thoennes@fau.de>
2020-12-18 08:14:30 +0100

    day 18.1

--- 2020/day18/day18.go
+++ 2020/day18/day18.go
@@ -4,75 +4,39 @@ import (
 	"adventofcode/utils"
 	"bufio"
 	"fmt"
+	"strconv"
 	"strings"
 )
 
-func calc(first int, second int, operator string) int {
-	if operator == "*" {
-		return first * second
-	} else {
-		return first + second
+func solveBlock(line string) string {
+
+	for {
+		closeBrace := strings.Index(line, ")")
+		if closeBrace == -1 {
+			break
+		}
+		openBrace := strings.LastIndex(line[:closeBrace], "(")
+		line = line[:openBrace] + solveBlock(line[openBrace+1:closeBrace]) + line[closeBrace+1:]
 	}
+	splitLine := strings.Split(line, " ")
+	current := utils.ToInt(splitLine[0])
+	for i := 0; i < len(splitLine)-2; i += 2 {
+		if splitLine[i+1] == "*" {
+			current *= utils.ToInt(splitLine[i+2])
+		} else {
+			current += utils.ToInt(splitLine[i+2])
+		}
+	}
+	return strconv.Itoa(current)
 }
 
 func main() {
-	scanner := bufio.NewScanner(utils.OpenFile("2020/day18/testinput"))
+	scanner := bufio.NewScanner(utils.OpenFile("2020/day18/input"))
 
 	sum := 0
-
 	for scanner.Scan() {
-		line := strings.Split(scanner.Text(), " ")
-		operator := false
-		operators := make([]string, 0)
-		braceCount := 0
-		braces := make([]int, 20)
-
-		popOperator := func() string {
-			ret := operators[len(operators)-1]
-			operators = operators[:len(operators)-1]
-			return ret
-		}
-		first := true
-
-		for _, elem := range line {
-			if operator {
-				operators = append(operators, elem)
-				operator = false
-			} else {
-				number := strings.Split(elem, "")
-				openB := strings.Count(elem, "(")
-				closeB := strings.Count(elem, ")")
-				if openB > 0 {
-					for i := 0; i < openB; i++ {
-						braceCount++
-						braces[braceCount] = utils.ToInt(number[len(number)-1])
-					}
-				} else if closeB > 0 {
-					for i := 0; i < closeB; i++ {
-						if i == 0 {
-							braces[braceCount] = calc(braces[braceCount], utils.ToInt(number[0]), popOperator())
-						}
-						braceCount--
-						if len(operators) == 0 {
-							braces[braceCount] = braces[braceCount+1]
-						} else {
-							braces[braceCount] = calc(braces[braceCount], braces[braceCount+1], popOperator())
-						}
-					}
-				} else {
-					if first {
-						braces[braceCount] = utils.ToInt(number[0])
-					} else {
-						braces[braceCount] = calc(braces[braceCount], utils.ToInt(number[0]), popOperator())
-					}
-
-				}
-				operator = true
-			}
-			first = false
-		}
-		sum += braces[0]
-		fmt.Println(braces[0])
+		line := scanner.Text()
+		sum += utils.ToInt(solveBlock(line))
 	}
 	fmt.Println(sum)
 }
