8ce4e44d61ddbb24cb4b560bbd1203db5b5bc934
Dominik Thoennes <dominik.thoennes@fau.de>
2020-12-19 10:11:44 +0100

    test input works

--- 2020/day19/day19.go
+++ 2020/day19/day19.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	scanner := bufio.NewScanner(utils.OpenFile("2020/day19/input"))
+	scanner := bufio.NewScanner(utils.OpenFile("2020/day19/testinput"))
 	//var regex string
 	rules := make(map[int]string)
 	messages := make([]string, 0)
@@ -30,24 +30,41 @@ func main() {
 			if isChar.MatchString(elem) {
 				newZeroRule = append(newZeroRule, elem)
 				continue
-			} else if elem == "(" || elem == ")" || elem == "|" {
+			} else if elem == "(" || elem == ")" || elem == "|" || elem == ")*" {
 				newZeroRule = append(newZeroRule, elem)
 				continue
 			}
 			changes = true
 			tmp := rules[utils.ToInt(elem)]
 			tmpSplit := strings.Split(tmp, " ")
-			if strings.Contains(tmp, "|") {
-				var newRule []string
+			var newRule = tmpSplit
+			if elem == "8" {
+				newRule = []string{"(", "42", ")*"}
+			} else if elem == "11" {
+				//newRule = []string{"(", "42", ")*", "(", "31", ")*"}
+				newRule = []string{"("}
+				for i := 1; i < 100; i++ {
+					tmp1 := make([]string, 0)
+					tmp2 := make([]string, 0)
+					for count := 1; count <= i; count++ {
+						tmp1 = append(tmp1, "42")
+						tmp2 = append(tmp2, "31")
+					}
+					newRule = append(newRule, tmp1...)
+					newRule = append(newRule, tmp2...)
+					if i != 99 {
+						newRule = append(newRule, "|")
+					}
+				}
+				newRule = append(newRule, ")")
+			} else if strings.Contains(tmp, "|") {
 				if len(tmpSplit) == 5 {
 					newRule = []string{"(", tmpSplit[0], tmpSplit[1], "|", tmpSplit[3], tmpSplit[4], ")"}
 				} else {
 					newRule = []string{"(", tmpSplit[0], "|", tmpSplit[2], ")"}
 				}
-				newZeroRule = append(newZeroRule, newRule...)
-			} else {
-				newZeroRule = append(newZeroRule, tmpSplit...)
 			}
+			newZeroRule = append(newZeroRule, newRule...)
 		}
 		zeroRule = newZeroRule
 		return changes
@@ -63,6 +80,7 @@ func main() {
 	}
 	newZeroRule = append(newZeroRule, "$")
 	regexStr := strings.Join(newZeroRule, "")
+	//fmt.Println(regexStr)
 	regex := regexp.MustCompile(regexStr)
 	count := 0
 	for _, elem := range messages {
