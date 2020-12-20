a6a367e85fb3a5f73c93fb6ef15822ec8c2ca4de
Dominik Thoennes <dominik.thoennes@fau.de>
2020-12-20 09:05:03 +0100

    day 20.1

--- 2020/day20/day20.go
+++ 2020/day20/day20.go
@@ -7,6 +7,36 @@ import (
 	"strings"
 )
 
+func rotateTile90(tile *[10][10]string) [10][10]string {
+	var rotate90 [10][10]string
+	for y := 0; y < 10; y++ {
+		for x := 0; x < 10; x++ {
+			rotate90[x][y] = tile[y][9-x]
+		}
+	}
+	return rotate90
+}
+
+func getAllVariants(tile *[10][10]string) [][10][10]string {
+	var rotate0 [10][10]string
+	var flipH [10][10]string
+	var flipV [10][10]string
+	for y := 0; y < 10; y++ {
+		for x := 0; x < 10; x++ {
+			rotate0[x][y] = tile[x][y]
+			flipH[x][y] = tile[x][9-y]
+			flipV[x][y] = tile[9-x][y]
+		}
+	}
+	ret := [][10][10]string{rotate0, flipH, flipV}
+	for i := 0; i <= 2; i++ {
+		ret = append(ret, rotateTile90(&ret[0+i*3]))
+		ret = append(ret, rotateTile90(&ret[1+i*3]))
+		ret = append(ret, rotateTile90(&ret[2+i*3]))
+	}
+	return ret
+}
+
 func getVariants(tile *[10][10]string) [][10][10]string {
 	//ret := make([][10][10]string,0)
 	var rotate0 [10][10]string
@@ -39,32 +69,46 @@ func sameStringSlice(one []string, two []string) bool {
 	return same
 }
 
-func findFourNeighbors(tiles [][10][10]string, tile *[10][10]string) bool {
-	top, bot, left, right := false, false, false, false
-	for _, elem := range tiles {
-		if elem == *tile {
-			continue
+func possibleNeighbors(tiles [][10][10]string, inputTile *[10][10]string, ids []int) [][]int {
+	neighborsAtOrientation := make([][]int, 0)
+	neighborsFound := 0
+	for _, tile := range getAllVariants(inputTile) {
+		neighborsSlice := make([]int, 0)
+		for otherId, elem := range tiles {
+			//fmt.Println("Other Tile:", ids[i])
+			if elem == tile {
+				continue
+			}
+			for _, otherTile := range getAllVariants(&elem) {
+				//top
+				if tile[:][0] == otherTile[:][0] {
+					neighborsSlice = append(neighborsSlice, ids[otherId])
+					neighborsFound++
+				}
+				//bot
+				if tile[:][9] == otherTile[:][9] {
+					neighborsSlice = append(neighborsSlice, ids[otherId])
+					neighborsFound++
+				}
+				//left
+				if sameStringSlice(tile[0][:], otherTile[0][:]) {
+					neighborsSlice = append(neighborsSlice, ids[otherId])
+					neighborsFound++
+				}
+				//right
+				if sameStringSlice(tile[9][:], otherTile[9][:]) {
+					neighborsSlice = append(neighborsSlice, ids[otherId])
+					neighborsFound++
+				}
+			}
 		}
-		//top := tile[:][0]
-		//bot := tile[:][9]
-		//left := tile[0][:]
-		//rigt := tile[9][:]
-		for _, otherTile := range getVariants(&elem) {
-			top = (tile[:][0] == otherTile[:][0]) || top
-			bot = (tile[:][9] == otherTile[:][9]) || bot
-			left = sameStringSlice(tile[0][:], otherTile[0][:]) || left
-			right = sameStringSlice(tile[9][:], otherTile[9][:]) || right
-		}
+		neighborsAtOrientation = append(neighborsAtOrientation, neighborsSlice)
 	}
-	if top && bot && left && right {
-		return true
-	} else {
-		return false
-	}
+	return neighborsAtOrientation
 }
 
 func main() {
-	scanner := bufio.NewScanner(utils.OpenFile("2020/day20/testinput"))
+	scanner := bufio.NewScanner(utils.OpenFile("2020/day20/input"))
 	tiles := make([][10][10]string, 0)
 	ids := make([]int, 0)
 	var newTile [10][10]string
@@ -89,11 +133,33 @@ func main() {
 
 	mult := 1
 	for i, tile := range tiles {
-		if !findFourNeighbors(tiles, &tile) {
+		//fmt.Println("Tile:,",ids[i])
+		tmp := possibleNeighbors(tiles, &tile, ids)
+		max := 0
+		for _, nbrs := range tmp {
+			tmpMap := make(map[int]bool)
+			for _, elem := range nbrs {
+				tmpMap[elem] = true
+			}
+			if len(tmpMap) > max {
+				max = len(tmpMap)
+			}
+		}
+		if max == 2 {
 			mult *= ids[i]
-			fmt.Println(i)
 		}
 	}
+	fmt.Println(mult)
+
+	//mult := 1
+	//for i, tile := range tiles {
+	//	if possibleNeighbors(tiles, &tile, ids) {
+	//		mult *= ids[i]
+	//		fmt.Println("True:",ids[i])
+	//	} else {
+	//		//fmt.Println("False:", ids[i])
+	//	}
+	//}
 
 	//for _, tile := range getVariants(&tiles[0]) {
 	//	utils.Print1010Grid(tile)
