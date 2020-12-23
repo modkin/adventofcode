2867a4b196773aa86fe7f55c9a91ff9e2cc30606
Dominik Thoennes <dominik.thoennes@fau.de>
2020-12-23 12:58:19 +0100

    might be getting closer

--- 2020/day23/day23.go
+++ 2020/day23/day23.go
@@ -17,9 +17,51 @@ func printRing(ring *ring.Ring) {
 	fmt.Println()
 }
 
+func playIntGame(cups []int, rounds int) {
+	amountOfCups := len(cups)
+	start := time.Now()
+	searchDest := 0
+	for i := 0; i < rounds; i++ {
+		pickup := utils.CopyIntSlice(cups[1:4])
+		cups = append(cups[:1], cups[4:]...)
+		destinationCup := 0
+		tmp := cups[0]
+		for {
+			tmp--
+			if tmp == 0 {
+				tmp = amountOfCups
+			}
+			if !utils.IntSliceContains(pickup, tmp) {
+				destinationCup = tmp
+				break
+			}
+		}
+		destinationIdx := 0
+		for cups[destinationIdx] != destinationCup {
+			destinationIdx++
+			searchDest++
+		}
+
+		tmpCups := append(cups[0:destinationIdx], pickup...)
+		cups = append(tmpCups, cups[destinationIdx:]...)
+		cups = append(cups[1:], cups[len(cups)-1])
+		if i%100 == 0 {
+			fmt.Println(i, searchDest, time.Now().Sub(start), destinationCup)
+			start = time.Now()
+			searchDest = 0
+		}
+	}
+}
+
 func playGame(cups *ring.Ring, rounds int) {
+	cups.Next()
+	previousPtr := cups
+	currentSafe := cups
+	dstPtr := cups
+	cups.Prev()
 
 	start := time.Now()
+	totalCups := cups.Len()
 	pickedUp := make([]int, 3)
 	searchDest := 0
 	searchNext := 0
@@ -31,34 +73,47 @@ func playGame(cups *ring.Ring, rounds int) {
 		//destinationCup := getOptimizedDestination(currentVal, pickup, )
 		destinationCup := 0
 
-		for i := range pickedUp {
-			pickedUp[i] = pickup.Value.(int)
+		for k := range pickedUp {
+			pickedUp[k] = pickup.Value.(int)
 			pickup = pickup.Next()
 		}
 		current := currentVal
 		for {
 			current--
 			if current == 0 {
-				current = 1000000
+				current = totalCups
 			}
 			if !utils.IntSliceContains(pickedUp, current) {
 				destinationCup = current
 				break
 			}
 		}
-		cups = cups.Move(-i)
-		for cups.Value.(int) != destinationCup {
-			cups = cups.Prev()
+		currentSafe = cups
+		//currentSafeAdr := &cups
+		//cups = cups.Move(-i)
+		//cups = previousPtr
+		for dstPtr.Value.(int) != destinationCup {
+			dstPtr = dstPtr.Next()
 			searchDest++
 		}
+		previousPtr = cups
 
-		cups.Link(pickup)
-		cups = cups.Move(i + 3)
+		dstPtr.Link(pickup)
+		dstPtr = dstPtr.Next().Next().Next()
+		if destinationCup%16 == 12 {
+			dstPtr = dstPtr.Next().Next().Next().Next()
+		}
+		//cups = currentSafe
+		//cups2 := *currentSafeAdr
+		if false {
+			fmt.Println(currentSafe)
+			fmt.Println(previousPtr)
+		}
 		for cups.Value.(int) != currentVal {
 			cups = cups.Next()
 			searchNext++
 		}
-		if i%1000 == 0 {
+		if searchDest > 0 || searchNext > 0 {
 
 			fmt.Println(i, searchDest, searchNext, time.Now().Sub(start), destinationCup)
 			start = time.Now()
@@ -96,22 +151,27 @@ func solve() {
 		cups = cups.Next()
 	}
 	fmt.Println("Task 23.1:", solution)
+	//playIntGame(cupsInt, 100)
+	//fmt.Println(cupsInt)
 
-	//cups2 := make([]int, 1000000)
+	cups2RingPtr := make([]*ring.Ring, 1000*1000)
+	cups2 := make([]int, 1000000)
 	cups2Ring := ring.New(1000000)
 	for i := 0; i < 1000000; i++ {
 		if i < len(cupsInt) {
-			//cups2[i] = cupsInt[i]
+			cups2[i] = cupsInt[i]
 			cups2Ring.Value = cupsInt[i]
 		} else {
-			//cups2[i] = i + 1
+			cups2[i] = i + 1
 			cups2Ring.Value = i + 1
 		}
+		cups2RingPtr[i] = cups2Ring
 		cups2Ring = cups2Ring.Next()
 	}
 	cups2Ring = cups2Ring.Prev()
 	start := time.Now()
-	playGame(cups2Ring, 1000*1000)
+	playGame(cups2Ring, 100)
+	//playIntGame(cups2, 1000*1000)
 	fmt.Println(time.Now().Sub(start))
 	for cups2Ring.Value.(int) != 1 {
 		cups2Ring = cups2Ring.Next()
