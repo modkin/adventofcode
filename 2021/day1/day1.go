363a50e266aebf154d7ef2f27a6150074418ac3a
Dominik Thoennes <dominik.thoennes@fau.de>
2021-11-30 22:30:52 +0100

    ready for day 1

--- 2021/day1/day1.go
+++ 2021/day1/day1.go
@@ -1,21 +1,21 @@
 package main
 
 import (
+	"adventofcode/utils"
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
 )
 
 func main() {
 
-	file, err := os.Open("2021/day1/input.txt")
+	file, err := os.Open("2021/day1/testinput")
 	if err != nil {
 		panic(err)
 	}
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		fmt.Println(strconv.Atoi(scanner.Text()))
+		fmt.Println(utils.ToInt(scanner.Text()))
 	}
 }
