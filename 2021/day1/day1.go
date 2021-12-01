f659eed2d2c981025072fd59d6489d9bdb80a714
Dominik Thoennes <dominik.thoennes@fau.de>
2021-12-01 06:03:42 +0100

    day 1.1 done

--- 2021/day1/day1.go
+++ 2021/day1/day1.go
@@ -9,13 +9,22 @@ import (
 
 func main() {
 
-	file, err := os.Open("2021/day1/testinput")
+	file, err := os.Open("2021/day1/input")
 	if err != nil {
 		panic(err)
 	}
 
 	scanner := bufio.NewScanner(file)
+	height := 0
+	//next := 0
+	count := 0
+	//meassures := make([]int,0)
 	for scanner.Scan() {
-		fmt.Println(utils.ToInt(scanner.Text()))
+		next := utils.ToInt(scanner.Text())
+		if next > height {
+			count++
+		}
+		height = next
 	}
+	fmt.Println(count - 1)
 }
