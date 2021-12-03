5666a78970d4839d679ac276c0103bfa2118268b
Dominik Thoennes <dominik.thoennes@fau.de>
2021-12-03 06:37:56 +0100

    worst solution EVA!!

--- 2021/day3/day3.go
+++ 2021/day3/day3.go
@@ -8,6 +8,33 @@ import (
 	"strings"
 )
 
+func getCommonBits(splitlines [][]string) ([]string, []string) {
+	gamma := make([]string, 0)
+	epsilon := make([]string, 0)
+	for i := 0; i < len(splitlines[0]); i++ {
+		ones := 0
+		zeros := 0
+		for _, splitline := range splitlines {
+			if splitline[i] == "0" {
+				zeros++
+			} else {
+				ones++
+			}
+		}
+		if ones > zeros {
+			gamma = append(gamma, "1")
+			epsilon = append(epsilon, "0")
+		} else if ones == zeros {
+			gamma = append(gamma, "1")
+			epsilon = append(epsilon, "0")
+		} else {
+			gamma = append(gamma, "0")
+			epsilon = append(epsilon, "1")
+		}
+	}
+	return gamma, epsilon
+}
+
 func main() {
 	file, err := os.Open("2021/day3/input")
 	if err != nil {
@@ -20,7 +47,6 @@ func main() {
 		line := strings.Split(scanner.Text(), "")
 		splitlines = append(splitlines, line)
 	}
-	fmt.Println(splitlines)
 	gamma := make([]string, 0)
 	epsilon := make([]string, 0)
 	for i := 0; i < len(splitlines[0]); i++ {
@@ -42,9 +68,48 @@ func main() {
 		}
 	}
 	gammaInt, _ := strconv.ParseInt(strings.Join(gamma[:], ""), 2, 64)
-	fmt.Println(gammaInt)
 	epsilonInt, _ := strconv.ParseInt(strings.Join(epsilon[:], ""), 2, 64)
-	fmt.Println(epsilonInt)
 	fmt.Println(gammaInt * epsilonInt)
 
+	cleanup := make([][]string, 0)
+	for _, splitline := range splitlines {
+		cleanup = append(cleanup, splitline)
+	}
+	for i := 0; i < len(splitlines[0]); i++ {
+		gamma, epsilon = getCommonBits(cleanup)
+		currentBit := gamma[i]
+		newCleanup := make([][]string, 0)
+		for _, splitline := range cleanup {
+			if splitline[i] == currentBit {
+				newCleanup = append(newCleanup, splitline)
+			}
+		}
+		cleanup = newCleanup
+	}
+	fmt.Println(cleanup)
+	oxygenGenRating, _ := strconv.ParseInt(strings.Join(cleanup[0][:], ""), 2, 64)
+	fmt.Println("OXY", oxygenGenRating)
+
+	cleanup = make([][]string, 0)
+	for _, splitline := range splitlines {
+		cleanup = append(cleanup, splitline)
+	}
+	for i := 0; i < len(splitlines[0]); i++ {
+		gamma, epsilon = getCommonBits(cleanup)
+		currentBit := epsilon[i]
+		newCleanup := make([][]string, 0)
+		for _, splitline := range cleanup {
+			if splitline[i] == currentBit {
+				newCleanup = append(newCleanup, splitline)
+			}
+		}
+		cleanup = newCleanup
+		if len(cleanup) == 1 {
+			break
+		}
+	}
+	fmt.Println(cleanup)
+	co2scrubrating, _ := strconv.ParseInt(strings.Join(cleanup[0][:], ""), 2, 64)
+	fmt.Println("CO2", co2scrubrating)
+	fmt.Println(oxygenGenRating * co2scrubrating)
 }
