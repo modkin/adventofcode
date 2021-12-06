b71b54ae680f6d36413ce8501035c9450b28bc3b
Dominik Thoennes <dominik.thoennes@fau.de>
2021-12-06 06:18:34 +0100

    day 6.2 done

--- 2021/day6/day6.go
+++ 2021/day6/day6.go
@@ -15,15 +15,17 @@ func main() {
 	}
 
 	fishes := make([]int, 0)
+	var birthday [9]int
 	scanner := bufio.NewScanner(file)
 	//lines := make([]ventline, 0)
 	for scanner.Scan() {
 		line := strings.Split(scanner.Text(), ",")
 		for _, l := range line {
 			fishes = append(fishes, utils.ToInt(l))
+			birthday[utils.ToInt(l)]++
 		}
 	}
-	for i := 0; i < 256; i++ {
+	for i := 0; i < 80; i++ {
 		for i2, fish := range fishes {
 			if fish == 0 {
 				fishes = append(fishes, 8)
@@ -32,7 +34,19 @@ func main() {
 				fishes[i2]--
 			}
 		}
-		fmt.Println(i)
 	}
-	fmt.Println(len(fishes))
+	fmt.Println("Day 6.1:", len(fishes))
+	for i := 0; i < 256; i++ {
+		tmp := birthday[0]
+		for i := 0; i <= 7; i++ {
+			birthday[i] = birthday[i+1]
+		}
+		birthday[8] = tmp
+		birthday[6] += tmp
+	}
+	sum := 0
+	for _, b := range birthday {
+		sum += b
+	}
+	fmt.Println("Day 6.2:", sum)
 }
