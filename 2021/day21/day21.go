a98defeffa08718770a4db7af0cde9529d7d5c14
Dominik Thoennes <dominik.thoennes@fau.de>
2021-12-21 10:07:43 +0100

    might be some progress or not

--- 2021/day21/day21.go
+++ 2021/day21/day21.go
@@ -5,6 +5,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strings"
 )
 
@@ -20,7 +21,18 @@ func roll(dice int) (int, int) {
 	return points, dice
 }
 
+func wrapTrack(pos int) int {
+	if pos > 10 {
+		pos = pos % 10
+		if pos == 0 {
+			pos = 10
+		}
+	}
+	return pos
+}
+
 func main() {
+	playerWins := make(map[[5]int][2]int)
 	file, err := os.Open("2021/day21/input")
 	scanner := bufio.NewScanner(file)
 	if err != nil {
@@ -28,12 +40,14 @@ func main() {
 	}
 	scanner.Scan()
 	line := strings.Split(scanner.Text(), " ")
-	player1 := utils.ToInt(line[len(line)-1])
+	p1 := utils.ToInt(line[len(line)-1])
 	scanner.Scan()
 	line = strings.Split(scanner.Text(), " ")
-	player2 := utils.ToInt(line[len(line)-1])
-	fmt.Println(player1, player2)
+	p2 := utils.ToInt(line[len(line)-1])
+	fmt.Println(p1, p2)
 	player1Points, player2Points := 0, 0
+	player1Start, player2Start := p1, p2
+	fmt.Println(player1Start, player2Start)
 
 	dice := 1
 	counter := 0
@@ -43,14 +57,9 @@ func main() {
 		fmt.Println(dice, player1Points, player2Points)
 		add, dice = roll(dice)
 		counter += 3
-		player1 += add
-		if player1 > 10 {
-			player1 = player1 % 10
-			if player1 == 0 {
-				player1 = 10
-			}
-		}
-		player1Points += player1
+		p1 += add
+		p1 = wrapTrack(p1)
+		player1Points += p1
 		if player1Points >= 1000 {
 			loser = player2Points
 			break
@@ -58,15 +67,10 @@ func main() {
 
 		add, dice = roll(dice)
 		counter += 3
-		player2 += add
-		if player2 > 10 {
-			player2 = player2 % 10
-			if player2 == 0 {
-				player2 = 10
-			}
-		}
+		p2 += add
+		p2 = wrapTrack(p2)
 
-		player2Points += player2
+		player2Points += p2
 		if player2Points >= 1000 {
 			loser = player1Points
 			break
@@ -76,4 +80,50 @@ func main() {
 
 	fmt.Println(loser, counter)
 	fmt.Println(loser * counter)
+
+	diceList := make([]int, 0)
+	for i := 1; i <= 3; i++ {
+		for j := 1; j <= 3; j++ {
+			for k := 1; k <= 3; k++ {
+				diceList = append(diceList, i+j+k)
+			}
+		}
+	}
+	sort.Ints(diceList)
+	fmt.Println(diceList)
+
+	for p1points := 20; p1points >= 0; p1points-- {
+		for p2points := 20; p2points >= 0; p2points-- {
+			for p1 = 1; p1 <= 10; p1++ {
+				for p2 = 1; p2 <= 10; p2++ {
+					for activePlayer := 1; activePlayer <= 2; activePlayer++ {
+						for _, d := range diceList {
+							if activePlayer == 1 {
+								tmp := playerWins[[5]int{p1, p2, p1points, p2points, activePlayer}]
+								if newPoints := wrapTrack(p1 + d); p1points+newPoints >= 21 {
+									tmp[0]++
+								} else {
+									tmp2 := playerWins[[5]int{newPoints, p2, p1points + newPoints, p2points, activePlayer}]
+									tmp[0] += tmp2[0]
+								}
+								playerWins[[5]int{p1, p2, p1points, p2points, activePlayer}] = tmp
+							} else {
+								tmp := playerWins[[5]int{p1, p2, p1points, p2points, activePlayer}]
+								if newPoints := wrapTrack(p2 + d); p2points+newPoints >= 21 {
+									tmp[1]++
+								} else {
+									tmp2 := playerWins[[5]int{newPoints, p2, p1points, p2points + newPoints, activePlayer}]
+									tmp[1] += tmp2[1]
+								}
+								playerWins[[5]int{p1, p2, p1points, p2points, activePlayer}] = tmp
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	fmt.Println(playerWins[[5]int{player1Start, player2Start, 0, 0, 1}])
+
 }
