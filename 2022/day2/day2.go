be885d0a2ba834256eb0ba3ace9668fb0aac8dc9
Dominik Thoennes <dominik.thoennes@fau.de>
2022-12-02 21:59:29 +0100

    different version

--- 2022/day2/day2.go
+++ 2022/day2/day2.go
@@ -23,54 +23,51 @@ func main() {
 		tmp := strings.Split(scanner.Text(), " ")
 		rounds = append(rounds, [2]rune{[]rune(tmp[0])[0], []rune(tmp[1])[0]})
 	}
-	fmt.Println(rounds)
 
 	points := 0
 	for i := 0; i < len(rounds); i++ {
 		points += pointMap[rounds[i][1]]
-		you := int(rounds[i][1])
-		other := int(rounds[i][0])
-		if you == 'X' && other == 'A' || you == 'Y' && other == 'B' || you == 'Z' && other == 'C' {
+		you := rounds[i][1]
+		other := rounds[i][0]
+		// distance is constant for draw
+		if you-other == 23 {
 			points += 3
 		} else {
-			if you == 'Y' && other == 'A' {
-				points += 6
-			} else if you == 'Z' && other == 'B' {
-				points += 6
-			} else if you == 'X' && other == 'C' {
+			if you == 'X' {
+				you += 3
+			}
+			result := you - other - 23
+			if result == 1 {
 				points += 6
 			}
 		}
 	}
 
 	pointTwoMap := map[rune]int{'X': 0, 'Y': 3, 'Z': 6}
-	pointChooseMap := map[rune]int{'A': 1, 'B': 2, 'C': 3}
 	pointsTwo := 0
 	for i := 0; i < len(rounds); i++ {
 		pointsTwo += pointTwoMap[rounds[i][1]]
 		you := rounds[i][1]
 		other := rounds[i][0]
 		if you == 'X' {
-			if other == 'A' {
-				pointsTwo += pointMap['Z']
-			} else if other == 'B' {
-				pointsTwo += pointMap['X']
-			} else if other == 'C' {
-				pointsTwo += pointMap['Y']
+			tmpPoints := pointMap[other+23] - 1
+			if tmpPoints == 0 {
+				tmpPoints = 3
 			}
+			pointsTwo += tmpPoints
 		} else if you == 'Y' {
-			pointsTwo += pointChooseMap[other]
+			pointsTwo += pointMap[other+23]
 		} else if you == 'Z' {
-			if other == 'A' {
-				pointsTwo += pointMap['Y']
-			} else if other == 'B' {
-				pointsTwo += pointMap['Z']
-			} else if other == 'C' {
-				pointsTwo += pointMap['X']
+			pointsTmp := pointMap[other+23] + 1
+			if pointsTmp == 4 {
+				pointsTmp = 1
 			}
+			pointsTwo += pointsTmp
 		}
 	}
-
+	if points != 11603 || pointsTwo != 12725 {
+		panic(err)
+	}
 	fmt.Println("Day 1.1:", points)
 	fmt.Println("Day 1.1:", pointsTwo)
 
