d7268735e18750f9d9b25938b22e0ced0415b3ad
Dominik Thoennes <dominik.thoennes@fau.de>
2022-12-02 06:19:06 +0100

    day 2.1

---
+++ 2022/day2/day1.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	file, err := os.Open("2022/day2/input")
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	rounds := make([][2]rune, 0)
+	pointMap := map[rune]int{'X': 1, 'Y': 2, 'Z': 3}
+
+	for scanner.Scan() {
+		tmp := strings.Split(scanner.Text(), " ")
+		rounds = append(rounds, [2]rune{[]rune(tmp[0])[0], []rune(tmp[1])[0]})
+	}
+	fmt.Println(rounds)
+
+	points := 0
+	for i := 0; i < len(rounds); i++ {
+		points += pointMap[rounds[i][1]]
+		you := int(rounds[i][1])
+		other := int(rounds[i][0])
+		if you == 'X' && other == 'A' || you == 'Y' && other == 'B' || you == 'Z' && other == 'C' {
+			points += 3
+		} else {
+			if you == 'Y' && other == 'A' {
+				points += 6
+			} else if you == 'Z' && other == 'B' {
+				points += 6
+			} else if you == 'X' && other == 'C' {
+				points += 6
+			}
+		}
+
+	}
+
+	fmt.Println("Day 1.1:", points)
+
+}
