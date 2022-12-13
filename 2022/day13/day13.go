4772b12826bfa2f69fbf2856243bbb156b76199d
Dominik Thoennes <dominik.thoennes@fau.de>
2022-12-13 11:26:21 +0100

    ...

--- 2022/day13/day13.go
+++ 2022/day13/day13.go
@@ -1,16 +1,129 @@
 package main
 
 import (
-	"adventofcode/utils"
 	"bufio"
 	"fmt"
+	"math"
 	"os"
+	"strconv"
 	"strings"
 )
+
+// return the index AFTER the closing ']'
+func findClosing(input string) int {
+	counter := 0
+	for idx, str := range input {
+		if str == '[' {
+			counter++
+		} else if str == ']' {
+			counter--
+		}
+		if counter == 0 {
+			return idx
+		}
+	}
+	return math.MaxInt
+}
 
 type recList struct {
 	val  int
 	list []recList
+}
+
+func compareInt(left int, right int) int {
+	if left < right {
+		return 1
+	} else if right < left {
+		return 2
+	} else if right == left {
+		return 3
+	} else {
+		fmt.Println("ERROR")
+		return math.MaxInt
+	}
+}
+
+// return
+// 1 : right order
+// 2 : wrong order
+// 3 continue
+func compare(left string, right string) int {
+	if left == "" && right != "" {
+		return 1
+	}
+	if right == "" && left != "" {
+		return 2
+	}
+	if right == "" && left == "" {
+		return 3
+	}
+	var leftNum, rightNum int
+	var err error
+	leftIsNum := false
+	if leftNum, err = strconv.Atoi(strings.Split(left, ",")[0]); err == nil {
+		leftIsNum = true
+	}
+	rightIsNum := false
+	if rightNum, err = strconv.Atoi(strings.Split(right, ",")[0]); err == nil {
+		rightIsNum = true
+	}
+	if leftIsNum && rightIsNum {
+		if ret := compareInt(leftNum, rightNum); ret != 3 {
+			return ret
+		}
+	}
+	leftIsList := false
+	var leftSub, rightSub string
+	if left[0] == '[' {
+		leftIsList = true
+		leftSub = left[1:findClosing(left)]
+		//left = left[strings.Index(left, "]")+2:]
+	}
+	rightIsList := false
+	if right[0] == '[' {
+		rightIsList = true
+		rightSub = right[1:findClosing(right)]
+		//right = right[strings.Index(right, "]")+2:]
+	}
+	if leftIsList && rightIsList {
+		ret := compare(leftSub, rightSub)
+		if ret != 3 {
+			return ret
+		}
+	}
+	if leftIsNum && rightIsList {
+		ret := compare(strconv.Itoa(leftNum), rightSub)
+		if ret != 3 {
+			return ret
+		}
+	}
+	if leftIsList && rightIsNum {
+		ret := compare(leftSub, strconv.Itoa(rightNum))
+		if ret != 3 {
+			return ret
+		}
+	}
+	nextLeft := strings.Join(strings.Split(left, ",")[1:], ",")
+	nextRight := strings.Join(strings.Split(right, ",")[1:], ",")
+	if nextLeft == "" && nextRight == "" {
+		return 3
+	}
+	if len(nextLeft) > 0 && nextLeft[0] == '[' {
+		if findClosing(nextLeft) == len(nextLeft) {
+			nextLeft = nextLeft[1 : len(nextLeft)-1]
+		}
+	}
+
+	if len(nextRight) > 0 && nextRight[0] == '[' {
+		if findClosing(nextRight) == len(nextRight) {
+			nextRight = nextRight[1 : len(nextRight)-1]
+		}
+	}
+	//if left == nextLeft && nextRight == right {
+	//	return 3
+	//}
+
+	return compare(nextLeft, nextRight)
 }
 
 func main() {
@@ -24,53 +137,17 @@ func main() {
 	rightOrder := 0
 	counter := 0
 
-outer:
 	for scanner.Scan() {
 		counter++
 		if scanner.Text() == "" {
 			scanner.Scan()
 		}
-		left := strings.Split(scanner.Text(), ",")
+		left := scanner.Text()
 		scanner.Scan()
-		right := strings.Split(scanner.Text(), ",")
-		//leftLevel := 0
-		//rightLevel := 0
-		for i := 0; ; i++ {
-			if i == len(left) {
-				rightOrder += counter
-				continue outer
-			}
-			if i == len(right) {
-				continue outer
-			}
-			leftCut := strings.Trim(left[i], "[")
-			leftCut = strings.Trim(leftCut, "]")
-			rightCut := strings.Trim(strings.Trim(right[i], "["), "]")
-			if leftCut == "" && rightCut == "" {
-				if strings.Count(left[i], "[") < strings.Count(right[i], "[") {
-					rightOrder += counter
-					continue outer
-				}
-				if strings.Count(left[i], "[") > strings.Count(right[i], "[") {
-					continue outer
-				}
-			}
-			if leftCut == "" {
-				rightOrder += counter
-				continue outer
-			}
-			if rightCut == "" {
-				rightOrder += counter
-				continue outer
-			}
-			leftNum := utils.ToInt(leftCut)
-			rightNum := utils.ToInt(rightCut)
-			if leftNum < rightNum {
-				rightOrder += counter
-				continue outer
-			} else if rightNum < leftNum {
-				continue outer
-			}
+		right := scanner.Text()
+		comp := compare(left[1:len(left)-1], right[1:len(right)-1])
+		if comp == 1 {
+			rightOrder += counter
 		}
 	}
 
