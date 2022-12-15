d2a72df04dc22d7f10ff6aee61457477702b9dde
Dominik Thoennes <dominik.thoennes@fau.de>
2022-12-15 07:00:00 +0100

    day 15.1 but too slow for day 2

--- 2022/day15/day15.go
+++ 2022/day15/day15.go
@@ -4,7 +4,6 @@ import (
 	"adventofcode/utils"
 	"bufio"
 	"fmt"
-	"math"
 	"os"
 	"strings"
 )
@@ -12,14 +11,16 @@ import (
 func main() {
 
 	file, err := os.Open("2022/day15/input")
+	//yCoord := 10
+	yCoord := 2000000
 	if err != nil {
 		panic(err)
 	}
 
-	grid := make(map[[2]int]string)
+	//grid := make(map[[2]int]string)
 	scanner := bufio.NewScanner(file)
+	noBeacon := make(map[int]string)
 
-	grid[[2]int{500, 0}] = "+"
 	for scanner.Scan() {
 		line := strings.Split(scanner.Text(), " ")
 		xSen := utils.ToInt(strings.Split(strings.Trim(line[2], ","), "=")[1])
@@ -28,52 +29,79 @@ func main() {
 		yBe := utils.ToInt(strings.Split(line[9], "=")[1])
 		xdist := utils.IntAbs(xBe - xSen)
 		ydist := utils.IntAbs(yBe - ySen)
-		grid[[2]int{xSen, ySen}] = "S"
-		walk := make(map[[2]int]bool)
-		walk[[2]int{xSen, ySen}] = true
+		totaldist := xdist + ydist
 
-		for i := 0; i <= xdist+ydist; i++ {
-			newWalk := make(map[[2]int]bool)
-			for pos := range walk {
-				grid[pos] = "#"
-				newWalk[[2]int{pos[0] + 1, pos[1]}] = true
-				newWalk[[2]int{pos[0], pos[1] + 1}] = true
-				newWalk[[2]int{pos[0] - 1, pos[1]}] = true
-				newWalk[[2]int{pos[0], pos[1] - 1}] = true
+		distLeft := totaldist - utils.IntAbs(yCoord-ySen)
+		for x := 0; x <= distLeft; x++ {
+			x1 := xSen + x
+			x2 := xSen - x
+			for _, testx := range []int{x1, x2} {
+				if _, ok := noBeacon[testx]; !ok {
+					if testx != xBe || yBe != yCoord {
+						noBeacon[testx] = "#"
+					} else {
+						noBeacon[testx] = "B"
+					}
+				}
 			}
-			walk = newWalk
-
 		}
-		grid[[2]int{xSen, ySen}] = "S"
-		grid[[2]int{xBe, yBe}] = "B"
+
+		//grid[[2]int{xSen, ySen}] = "S"
+		//walk := make(map[[2]int]bool)
+		//walk[[2]int{xSen, ySen}] = true
+		//
+		//for i := 0; i <= xdist+ydist; i++ {
+		//	newWalk := make(map[[2]int]bool)
+		//	for pos := range walk {
+		//		grid[pos] = "#"
+		//		newWalk[[2]int{pos[0] + 1, pos[1]}] = true
+		//		newWalk[[2]int{pos[0], pos[1] + 1}] = true
+		//		newWalk[[2]int{pos[0] - 1, pos[1]}] = true
+		//		newWalk[[2]int{pos[0], pos[1] - 1}] = true
+		//	}
+		//	walk = newWalk
+		//
+		//}
+		//grid[[2]int{xSen, ySen}] = "S"
+		//grid[[2]int{xBe, yBe}] = "B"
 		//utils.Print2DStringsGrid(grid)
 		fmt.Println("new Line")
 	}
-	xMin, yMin, xMax, yMax := math.MaxInt, math.MaxInt, 0, 0
-	for i := range grid {
-		if i[0] < xMin {
-			xMin = i[0]
-		}
-		if i[0] > xMax {
-			xMax = i[0]
-		}
-		if i[1] < yMin {
-			yMin = i[1]
-		}
-		if i[1] > yMax {
-			yMax = i[1]
+	//xMin, yMin, xMax, yMax := math.MaxInt, math.MaxInt, 0, 0
+	//for i := range grid {
+	//	if i[0] < xMin {
+	//		xMin = i[0]
+	//	}
+	//	if i[0] > xMax {
+	//		xMax = i[0]
+	//	}
+	//	if i[1] < yMin {
+	//		yMin = i[1]
+	//	}
+	//	if i[1] > yMax {
+	//		yMax = i[1]
+	//	}
+	//}
+	//
+	//counter := 0
+	//
+	//for i := xMin; i < xMax; i++ {
+	//	fmt.Print(grid[[2]int{i, yCoord}])
+	//	if grid[[2]int{i, yCoord}] == "#" {
+	//		counter++
+	//	}
+	//}
+	//fmt.Println()
+	//for i := xMin; i < xMax; i++ {
+	//	fmt.Print(noBeacon[i])
+	//}
+	//fmt.Println(counter)
+	counter2 := 0
+	for _, s := range noBeacon {
+		if s == "#" {
+			counter2++
 		}
 	}
-
-	counter := 0
-	yCoord := 2000000
-	for i := xMin; i < xMax; i++ {
-		//fmt.Print(grid[[2]int{i, yCoord}])
-		if grid[[2]int{i, yCoord}] == "#" {
-			counter++
-		}
-	}
-	fmt.Println()
-	fmt.Println(counter)
+	fmt.Println(counter2)
 
 }
