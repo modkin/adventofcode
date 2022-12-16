ca1b331b8df4bf7af187dfd090acb446845038c3
Dominik Thoennes <dominik.thoennes@fau.de>
2022-12-16 12:13:59 +0100

    current state ok slow

--- 2022/day16/day16.go
+++ 2022/day16/day16.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"os"
 	"regexp"
-	"sort"
 	"strconv"
 	"strings"
 )
@@ -19,16 +18,16 @@ type valve struct {
 type state struct {
 	open    []string
 	flow    int
-	current string
+	me      string
+	elefant string
 	time    int
-	path    []string
 }
 
 func findMaxState(input []state) state {
 	max := 0
 	var s state
 	for _, i2 := range input {
-		if i2.time <= 30 {
+		if i2.time <= 26 {
 			if i2.flow > max {
 				max = i2.flow
 				s = i2
@@ -40,7 +39,7 @@ func findMaxState(input []state) state {
 
 func isUseless(new state, states []state) bool {
 	for _, s := range states {
-		if new.current == s.current {
+		if new.me == s.me {
 			if new.flow < s.flow && new.time > s.time {
 				return true
 			}
@@ -50,7 +49,8 @@ func isUseless(new state, states []state) bool {
 }
 
 func isUseless2(new state, states map[string][]state) bool {
-	for _, s := range states[new.current] {
+	combinedPos := strings.Join([]string{new.me, new.elefant}, "-")
+	for _, s := range states[combinedPos] {
 		if new.flow < s.flow && new.time > s.time {
 			return true
 		}
@@ -61,7 +61,7 @@ func isUseless2(new state, states map[string][]state) bool {
 
 func getCheckSum(s state) string {
 	ret := strings.Join(s.open, "-")
-	ret += "-" + s.current + "-"
+	ret += "-" + s.me + "-" + s.elefant + "-"
 	ret += strconv.Itoa(s.time)
 	ret += "-" + strconv.Itoa(s.flow)
 	return ret
@@ -69,7 +69,7 @@ func getCheckSum(s state) string {
 
 func run() {
 
-	file, err := os.Open("/Users/dominikthoennes/go/src/adventofcode/2022/day16/input")
+	file, err := os.Open("/Users/dominikthoennes/go/src/adventofcode/2022/day16/testinput")
 
 	if err != nil {
 		panic(err)
@@ -80,6 +80,7 @@ func run() {
 	allState := make([]state, 0)
 	alreadyChecked := make(map[string]bool)
 	stateAtPos := make(map[string][]state)
+	const maxTime = 26
 
 	re := regexp.MustCompile(`\d*;`)
 	for scanner.Scan() {
@@ -94,44 +95,53 @@ func run() {
 
 	}
 	fmt.Println(allValves)
-	allState = append(allState, state{[]string{"AA"}, 0, "AA", 1, make([]string, 0)})
+	allState = append(allState, state{[]string{"AA"}, 0, "AA", "AA", 1})
 	newOpen := true
 	currentMax := 0
 	for newOpen {
 		newOpen = false
 		newAllState := make([]state, 0)
-		for _, maxState := range allState {
-			max := findMaxState(newAllState).flow
-			if max > currentMax {
-				currentMax = max
-				fmt.Println(currentMax, len(allState))
-			}
-			if maxState.time >= 30 {
+		for _, curState := range allState {
+
+			if curState.time >= maxTime {
 				continue
 			}
 			newOpen = true
 
-			for _, v := range allValves[maxState.current].tunnel {
-				newPath := append(utils.CopyStringSlice(maxState.path), v)
-				newState := state{utils.CopyStringSlice(maxState.open), maxState.flow, v, maxState.time + 1, newPath}
-				if !utils.SliceContains(newState.open, v) && allValves[v].flow != 0 {
-					newState2 := state{utils.CopyStringSlice(maxState.open), maxState.flow, v, maxState.time + 1, newPath}
-					tmp := append(newState2.open, v)
-					sort.Strings(tmp)
-					newState2.open = tmp
-					newState2.flow += (30 - newState2.time) * allValves[v].flow
-					newState2.time++
-					newAllState = append(newAllState, newState2)
+			for _, newOwnPos := range allValves[curState.me].tunnel {
+				newState := state{utils.CopyStringSlice(curState.open), curState.flow, newOwnPos, curState.elefant, curState.time + 1}
+				for _, newElefantPos := range allValves[curState.elefant].tunnel {
+					newState.elefant = newElefantPos
+
+					if !utils.SliceContains(newState.open, newOwnPos) && allValves[newOwnPos].flow != 0 {
+						for _, newNewElefantPos := range allValves[newElefantPos].tunnel {
+							newState2 := state{append(utils.CopyStringSlice(curState.open), newOwnPos), curState.flow, newOwnPos, newNewElefantPos, curState.time + 1}
+							newState2.flow += (maxTime - newState2.time) * allValves[newOwnPos].flow
+							newState2.time++
+							newAllState = append(newAllState, newState2)
+						}
+					}
+					if !utils.SliceContains(newState.open, newElefantPos) && allValves[newElefantPos].flow != 0 {
+						for _, newNewOwnPos := range allValves[newOwnPos].tunnel {
+							newState2 := state{append(utils.CopyStringSlice(curState.open), newElefantPos), curState.flow, newNewOwnPos, newElefantPos, curState.time + 1}
+							newState2.flow += (maxTime - newState2.time) * allValves[newElefantPos].flow
+							newState2.time++
+							newAllState = append(newAllState, newState2)
+						}
+					}
+
+					newAllState = append(newAllState, newState)
 				}
-				newAllState = append(newAllState, newState)
 			}
+
 		}
 		allState = make([]state, 0)
 		for _, s := range newAllState {
-			if _, ok := stateAtPos[s.current]; ok {
-				stateAtPos[s.current] = append(stateAtPos[s.current], s)
+			combinedPos := strings.Join([]string{s.me, s.elefant}, "-")
+			if _, ok := stateAtPos[combinedPos]; ok {
+				stateAtPos[combinedPos] = append(stateAtPos[s.me], s)
 			} else {
-				stateAtPos[s.current] = []state{s}
+				stateAtPos[combinedPos] = []state{s}
 			}
 
 		}
@@ -141,6 +151,10 @@ func run() {
 				if _, ok := alreadyChecked[getCheckSum(s)]; !ok {
 					allState = append(allState, s)
 					alreadyChecked[getCheckSum(s)] = true
+					if s.flow > currentMax {
+						currentMax = s.flow
+						fmt.Println(currentMax, len(allState))
+					}
 				}
 			}
 		}
