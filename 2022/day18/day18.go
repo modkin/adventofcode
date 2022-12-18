be3ce97f17a90e3ae3482b594f9b3fc3c8bb7c52
Dominik Thoennes <dominik.thoennes@fau.de>
2022-12-18 09:07:09 +0100

    day 18.2; not the fastest

--- 2022/day18/day18.go
+++ 2022/day18/day18.go
@@ -8,6 +8,37 @@ import (
 	"strings"
 )
 
+func max(in [3]int) int {
+	maxi := 0
+	for _, i := range in {
+		if i > maxi {
+			maxi = i
+		}
+	}
+	return maxi
+}
+
+func getNeighbors(in [3]int) [][3]int {
+	out := make([][3]int, 0)
+	for idx := 0; idx < 3; idx++ {
+		for _, step := range []int{-1, 1} {
+			nbr := in
+			nbr[idx] += step
+			out = append(out, nbr)
+		}
+	}
+	return out
+}
+
+func isBoundary(in [3]int, max int) bool {
+	for _, i := range in {
+		if i == 0 || i == max {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
 	file, err := os.Open("2022/day18/input")
 
@@ -16,150 +47,62 @@ func main() {
 	}
 
 	cubes := make(map[[3]int]bool)
-	initCubes := make(map[[3]int]bool)
-	airInside := make(map[[3]int]bool)
+	airAround := make([][3]int, 0)
 
 	scanner := bufio.NewScanner(file)
+
+	maximum := 0
 	for scanner.Scan() {
 		line := strings.Split(scanner.Text(), ",")
 		newCube := [3]int{utils.ToInt(line[0]), utils.ToInt(line[1]), utils.ToInt(line[2])}
+		if tmp := max(newCube); tmp > maximum {
+			maximum = tmp
+		}
 		cubes[newCube] = true
-		initCubes[newCube] = true
 	}
-	//fmt.Println(cubes)
-
-	counter := 0
-	counter2 := 0
-	max := 20
-	allPoint := make([][3]int, 0)
-	for x := 0; x < 20; x++ {
-		for y := 0; y < 20; y++ {
-			for z := 0; z < 20; z++ {
-				allPoint = append(allPoint, [3]int{x, y, z})
-			}
-		}
-	}
-	fmt.Println("Num Cubes 1:", len(cubes))
-	fmt.Println("len all point: ", len(allPoint))
-
-	checkDir := false
-	xf, yf, zf := true, true, true
-	outside := 0
-
-	for _, c := range allPoint {
-		if _, ok := cubes[c]; ok {
-			continue
-		}
-		xf, yf, zf = true, true, true
-		checkDir = false
-		for x := 0; x > -max; x-- {
-			check := [3]int{c[0] + x, c[1], c[2]}
-			if _, ok := cubes[check]; ok {
-				checkDir = true
-			}
-		}
-		if !checkDir {
-			xf = false
-		}
-		checkDir = false
-		for x := 0; x < max; x++ {
-			check := [3]int{c[0] + x, c[1], c[2]}
-			if _, ok := cubes[check]; ok {
-				checkDir = true
-			}
-		}
-		if !checkDir {
-			xf = false
-		}
-		checkDir = false
-		for y := 0; y > -max; y-- {
-			check := [3]int{c[0] + y, c[1], c[2]}
-			if _, ok := cubes[check]; ok {
-				checkDir = true
-			}
-		}
-		if !checkDir {
-			yf = false
-		}
-		checkDir = false
-		for y := 0; y < max; y++ {
-			check := [3]int{c[0] + y, c[1], c[2]}
-			if _, ok := cubes[check]; ok {
-				checkDir = true
-			}
-		}
-		if !checkDir {
-			yf = false
-		}
-		checkDir = false
-		for z := 0; z > -max; z-- {
-			check := [3]int{c[0] + z, c[1], c[2]}
-			if _, ok := cubes[check]; ok {
-				checkDir = true
-			}
-		}
-		if !checkDir {
-			zf = false
-		}
-		checkDir = false
-		for z := 0; z < max; z++ {
-			check := [3]int{c[0] + z, c[1], c[2]}
-			if _, ok := cubes[check]; ok {
-				checkDir = true
-			}
-		}
-		if !checkDir {
-			zf = false
-		}
-		if xf && yf && zf {
-			airInside[c] = true
-			//cubes[c] = true
-			counter2++
-			if _, ok := initCubes[c]; ok {
-				fmt.Println("ERROR")
-			}
-		} else {
-			outside++
-		}
-	}
-
-	fmt.Println("Num cubes 2:", len(cubes))
-	fmt.Println("Num air:", len(airInside))
-	fmt.Println("outside:", outside)
-	fmt.Println("init cubes:", len(initCubes))
-	fmt.Println("all:", len(allPoint))
-	fmt.Println(len(allPoint) - len(airInside) - outside - len(initCubes))
+	maximum = maximum + 1
 
 	for c := range cubes {
-		for _, x := range []int{-1, 1} {
-			check := [3]int{c[0] + x, c[1], c[2]}
-			if _, ok := cubes[check]; !ok {
-				if _, ok2 := airInside[check]; !ok2 {
-					counter++
+		for idx := 0; idx < 3; idx++ {
+			for _, step := range []int{-1, 1} {
+				check := c
+				check[idx] += step
+				if _, ok := cubes[check]; !ok {
+					airAround = append(airAround, check)
 				}
 			}
 		}
-		for _, y := range []int{-1, 1} {
-			check := [3]int{c[0], c[1] + y, c[2]}
-			if _, ok := cubes[check]; !ok {
-				if _, ok2 := airInside[check]; !ok2 {
-					counter++
-				}
-			}
-		}
-		for _, z := range []int{-1, 1} {
-			check := [3]int{c[0], c[1], c[2] + z}
-			if _, ok := cubes[check]; !ok {
-				if _, ok2 := airInside[check]; !ok2 {
-					counter++
-				}
-			}
-
-		}
-
 	}
 
-	fmt.Println(counter2)
-	fmt.Println(counter)
-	//fmt.Println(counter - counter2*6)
+	fmt.Println("Day 18.1:", len(airAround))
+
+	airOutside := make([][3]int, 0)
+airLoop:
+	for _, air := range airAround {
+		curPos := [][3]int{air}
+		visited := map[[3]int]bool{air: true}
+
+		for len(curPos) > 0 {
+			nextPos := make([][3]int, 0)
+			for _, pos := range curPos {
+				nbrs := getNeighbors(pos)
+				for _, nbr := range nbrs {
+					if isBoundary(nbr, maximum) {
+						airOutside = append(airOutside, air)
+						continue airLoop
+					} else {
+						if _, islava := cubes[nbr]; !islava {
+							if _, ok := visited[nbr]; !ok {
+								nextPos = append(nextPos, nbr)
+								visited[nbr] = true
+							}
+						}
+					}
+				}
+			}
+			curPos = nextPos
+		}
+	}
+	fmt.Println("Day 18.2:", len(airOutside))
+
 }
