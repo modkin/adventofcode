7e68fdfabee3fa336a33f11c3106eda0b869553a
Dominik Thoennes <dominik.thoennes@fau.de>
2023-12-03 06:50:37 +0100

    day 3 ugly

--- 2023/day3/day3.go
+++ 2023/day3/day3.go
@@ -55,7 +55,62 @@ func main() {
 		}
 	}
 
-	fmt.Println(allNumbers)
+	//fmt.Println(allNumbers)
 	sum := utils.SumSlice(allNumbers)
 	fmt.Println("Day 3.1:", sum)
+
+	var gearRatios []int
+	for lineIdx, line := range lines {
+		for charIdx, char := range line {
+			if char == '*' {
+				var adjacentNumbers []int
+				for x := -1; x <= 1; x++ {
+					for y := -1; y <= 1; y++ {
+						if x+lineIdx < 0 || x+lineIdx >= len(lines) || y+charIdx < 0 || y+charIdx >= len(lines[0]) {
+							continue
+						}
+						if unicode.IsDigit(rune(lines[lineIdx+x][charIdx+y])) {
+							ytmp := y
+							number = ""
+							for i := -2; i <= 2; i++ {
+								if i+charIdx < 0 || y+charIdx > len(lines[0])-1 {
+									continue
+								}
+
+								adjacentChar := lines[lineIdx+x][charIdx+ytmp+i]
+								if unicode.IsDigit(rune(adjacentChar)) {
+									number += string(adjacentChar)
+									if i >= 0 {
+										y++
+									}
+								} else {
+									if i < 0 && number != "" {
+										number = ""
+									}
+									if i > 0 && number != "" {
+										break
+									}
+								}
+							}
+							adjacentNumbers = append(adjacentNumbers, utils.ToInt(number))
+						}
+
+					}
+				}
+				fmt.Println(adjacentNumbers)
+				if len(adjacentNumbers) == 2 {
+					gearRatios = append(gearRatios, adjacentNumbers[0]*adjacentNumbers[1])
+				}
+			} else {
+				if adjacent && number != "" {
+					allNumbers = append(allNumbers, utils.ToInt(number))
+				}
+				number = ""
+				adjacent = false
+			}
+		}
+	}
+	//fmt.Println(gearRatios)
+	sum2 := utils.SumSlice(gearRatios)
+	fmt.Println("Day 3.2:", sum2)
 }
