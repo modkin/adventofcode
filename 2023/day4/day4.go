8ed2a912e36377ec77e2ab08a35f8d690895093b
Dominik Thoennes <dominik.thoennes@fau.de>
2023-12-04 06:27:43 +0100

    day 4 done

--- 2023/day4/day4.go
+++ 2023/day4/day4.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"adventofcode/utils"
 	"bufio"
 	"fmt"
 	"os"
@@ -15,8 +16,6 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
-	//var lines []string
-
 	var lines []string
 	for scanner.Scan() {
 		lines = append(lines, scanner.Text())
@@ -25,22 +24,25 @@ func main() {
 
 	totalPoints := 0
 
-	//var multiplier []int
-	idx := 1
-	for _, line := range lines {
+	multiplier := make([]int, len(lines))
+	for i, _ := range multiplier {
+		multiplier[i] = 1
+	}
+
+	for idx, line := range lines {
+		offset := 1
 		split := strings.Split(line, ":")
 		winMine := strings.Split(split[1], "|")
 		var win []string
-		//var mine []string
 		CardPoints := 0
-		//multiplierIdx := idx
-		for _, s := range strings.Fields(strings.TrimSpace(winMine[1])) {
+		for _, s := range strings.Fields(strings.TrimSpace(winMine[0])) {
 			win = append(win, s)
 		}
-		for _, s := range strings.Split(winMine[0], " ") {
+		for _, s := range strings.Split(winMine[1], " ") {
 			for _, s2 := range win {
 				if s == s2 {
-
+					multiplier[idx+offset] += multiplier[idx]
+					offset++
 					if CardPoints == 0 {
 						CardPoints = 1
 					} else {
@@ -50,10 +52,9 @@ func main() {
 			}
 		}
 		totalPoints += CardPoints
-		idx++
 	}
 
-	fmt.Println(totalPoints)
-	//sum := utils.SumSlice(lines)
-	//fmt.Println("Day 3.2:", sum)
+	fmt.Println("Day 4.1:", totalPoints)
+	sum := utils.SumSlice(multiplier)
+	fmt.Println("Day 4.2:", sum)
 }
