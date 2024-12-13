8f64dc3491f274d8eca3de1221b29ed8e73caec7
Dominik Thoennes <dominik.thoennes@fau.de>
2024-12-13 09:28:16 +0100

    cleanup

--- 2024/day13/day13.go
+++ 2024/day13/day13.go
@@ -3,14 +3,33 @@ package main
 import (
 	"adventofcode/utils"
 	"fmt"
-	"math"
 	"regexp"
 )
 
 type machine struct {
-	a     [2]int
-	b     [2]int
-	price [2]int
+	a      [2]int
+	b      [2]int
+	price  [2]int
+	price2 [2]int
+}
+
+func solve(ma machine, part1 bool) (bool, [2]int) {
+	var p [2]int
+	if part1 {
+		p = ma.price
+	} else {
+		p = ma.price2
+	}
+	det := ma.a[0]*ma.b[1] - ma.a[1]*ma.b[0]
+	tmp1 := p[0]*ma.b[1] - ma.b[0]*p[1]
+	tmp2 := ma.a[0]*p[1] - p[0]*ma.a[1]
+	aPush := tmp1 / det
+	bPush := tmp2 / det
+	if float64(aPush) == float64(tmp1)/float64(det) && float64(bPush) == float64(tmp2)/float64(det) {
+		return true, [2]int{aPush, bPush}
+	} else {
+		return false, [2]int{aPush, bPush}
+	}
 }
 
 func main() {
@@ -28,34 +47,24 @@ func main() {
 		b := [2]int{utils.ToInt(coordsB[1]), utils.ToInt(coordsB[2])}
 		coordsPrice := regPrice.FindStringSubmatch(lines[i+2])
 		price := [2]int{utils.ToInt(coordsPrice[1]), utils.ToInt(coordsPrice[2])}
-		newMach := machine{a: a, b: b, price: price}
+		price2 := [2]int{utils.ToInt(coordsPrice[1]) + 10000000000000, utils.ToInt(coordsPrice[2]) + 10000000000000}
+		newMach := machine{a: a, b: b, price: price, price2: price2}
 		allMachines = append(allMachines, newMach)
 	}
-
-	sum := 0
-
-	//ma := allMachines[0]
-	//xCoord := ma.a[0]*80 + ma.b[0]*40
-	//yCoord := ma.a[1]*80 + ma.b[1]*40
-	//fmt.Println(ma.price == [2]int{xCoord, yCoord})
+	sum1 := 0
+	sum2 := 0
 
 	for _, ma := range allMachines {
-		minPush := math.MaxInt
-		for a := 0; a <= 100; a++ {
-			for b := 0; b <= 100; b++ {
-				xCoord := ma.a[0]*a + ma.b[0]*b
-				yCoord := ma.a[1]*a + ma.b[1]*b
-				if ma.price == [2]int{xCoord, yCoord} {
-					if cost := a*3 + b; cost < minPush {
-						minPush = cost
-						fmt.Println(a, b)
-					}
-				}
-			}
+		intSol, pushes := solve(ma, true)
+		if intSol {
+			sum1 += pushes[0]*3 + pushes[1]
 		}
-		if minPush != math.MaxInt {
-			sum += minPush
+
+		intSol, pushes = solve(ma, false)
+		if intSol {
+			sum2 += pushes[0]*3 + pushes[1]
 		}
 	}
-	fmt.Println(sum)
+	fmt.Println("Day 13.1:", sum1)
+	fmt.Println("Day 13.2:", sum2)
 }
